fa49182e69429e7e8254249e8410d5b1bc4b1058
Thebestscripter3 <thomasalfonsosos@gmail.com>
2026-09-23 22:07:53 -0500

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjRmNTZhYjg1LWUwZWYtNDVjOS04MGY0LWRlOTI2NTg4NjQwZCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJuZXd1aSJ9fQ"))()
+ID="4f56ab85-e0ef-45c9-80f4-de926588640d";
+RUN_AFTER=loadstring(game:HttpGet("https://cdn.project-reverse.org/mm2_visual_new_ui.luau"));
+loadstring(game:HttpGet("https://cdn.project-reverse.org/loader.luau"))();
